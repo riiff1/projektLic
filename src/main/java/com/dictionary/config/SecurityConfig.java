a9778ac087d1003f79cbc4866e0afc09fe838d32
riiff1 <riiff@wp.pl>
2018-04-08 18:02:45 +0200

    Added simple html files and edited pom for angular,jQuery and bootstrap. Added configuration to project

---
+++ src/main/java/com/dictionary/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.dictionary.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/index").permitAll()
+                .and().authorizeRequests().antMatchers("/regiser").permitAll()
+                .and().authorizeRequests().antMatchers("/logout").permitAll()
+                .and().formLogin().loginPage("/login").usernameParameter("nickName").passwordParameter("password")
+                .and().logout().logoutSuccessUrl("/logoutt")
+                .and().authorizeRequests().antMatchers("/dictionary").authenticated();
+        http.csrf().disable();
+    }
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select USER_NAME, PASSWORD, ENABLED from TBL_USER where USER_NAME = ?")
+                .authoritiesByUsernameQuery("select u.USER_NAME, r.ROLE " +
+                        "from TBL_USER_ROLE r " +
+                        "  JOIN TBL_USER u ON r.USER_ID_FK = u.USER_ID " +
+                        "where u.USER_NAME = ?").passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/dictionary/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.dictionary.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.dictionary")
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
