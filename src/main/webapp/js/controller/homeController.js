ceacbf442d8e648c206f5ec8fb691b26aed8ac5e
riiff1 <riiff@wp.pl>
2018-04-15 23:14:19 +0200

    Added search function for dictionary

--- src/main/webapp/js/controller/homeController.js
+++ src/main/webapp/js/controller/homeController.js
@@ -1,5 +1,5 @@
-var app = angular.module('myApp',[]);
-app.controller('homeController', ['$scope', 'homeService', function ($scope, homeService) {
+var app = angular.module('myApp',['ui.bootstrap']);
+app.controller('homeController', ['$scope', 'homeService', function ($scope, homeService, $log) {
 
     $scope.setScope = function(){
         homeService.setScope($scope);
@@ -8,6 +8,26 @@ app.controller('homeController', ['$scope', 'homeService', function ($scope, hom
 
     $scope.pickedSpecializationFunction = function (specialization) {
         homeService.pickedSpecializationFunction(specialization);
-    }
+    };
+
+    $scope.pickedWordFromSearch = function ($item) {
+        console.log("item");
+        console.log($item);
+    };
+
+    $scope.totalItems = 64;
+    $scope.currentPage = 4;
+
+    $scope.setPage = function (pageNo) {
+        $scope.currentPage = pageNo;
+    };
+
+    $scope.pageChanged = function() {
+        $log.log('Page changed to: ' + $scope.currentPage);
+    };
+
+    $scope.maxSize = 5;
+    $scope.bigTotalItems = 175;
+    $scope.bigCurrentPage = 1;
 
 }]);
