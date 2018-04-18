362143c1f70a84fa1155a89292d8881d4a33919e
riiff1 <riiff@wp.pl>
2018-04-18 23:04:00 +0200

    Finished and fixed all issues on home page

--- src/main/webapp/js/controller/homeController.js
+++ src/main/webapp/js/controller/homeController.js
@@ -31,19 +31,4 @@ app.controller('homeController', ['$scope', 'homeService', function ($scope, hom
         homeService.addNote(dictionaryAndNote);
     };
 
-    $scope.totalItems = 64;
-    $scope.currentPage = 4;
-
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
-
-    $scope.pageChanged = function() {
-        $log.log('Page changed to: ' + $scope.currentPage);
-    };
-
-    $scope.maxSize = 5;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
-
 }]);
