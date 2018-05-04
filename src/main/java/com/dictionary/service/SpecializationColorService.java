e7f7ef101a1dcebcc5ed1e6ee10d0cb805aca163
riiff1 <riiff@wp.pl>
2018-05-04 23:40:46 +0200

    Show/Hide colors button

--- src/main/java/com/dictionary/service/SpecializationColorService.java
+++ src/main/java/com/dictionary/service/SpecializationColorService.java
@@ -3,9 +3,11 @@ package com.dictionary.service;
 import com.dictionary.dao.SpecializationColorDao;
 import com.dictionary.dao.SpecializationDao;
 import com.dictionary.dto.SpecializationColorDto;
+import com.dictionary.dto.SpecializationDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,7 +24,11 @@ public class SpecializationColorService {
     
     public List<SpecializationColorDto> getAvailableSpecializationByUser() {
         long userId = userService.getLoggedUserId();
-        return specializationColorDao.getAvailableSpecializationByUser(userId, specializationDao.getAvailableSpecializationByUser(userId));
+        List<SpecializationDto> availableSpecialization = specializationDao.getAvailableSpecializationByUser(userId);
+        if(availableSpecialization.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return specializationColorDao.getAvailableSpecializationByUser(userId, availableSpecialization);
     }
 
     public void updateSpecializationColor(List<SpecializationColorDto> specializationColorDto) {
--- src/main/webapp/js/service/scheduleService.js
+++ src/main/webapp/js/service/scheduleService.js
@@ -17,66 +17,70 @@ app.service('scheduleService',['$http','$window', function ($http, $window) {
             .then(function (response) {
                 specData = response.data;
                 self.scope.eventSpecialization = response.data;
+                if(response.data.length != 0) {
+                    self.scope.isSpecialization = true;
+                    //pobranie eventow
+                    $http.get("/event/allEventsForAllSpecializationByUser")
+                        .then(function (response) {
+                            angular.forEach(response.data, function (value) {
 
-                //pobranie eventow
-                $http.get("/event/allEventsForAllSpecializationByUser")
-                    .then(function (response) {
-                        angular.forEach(response.data, function (value) {
+                                var colorSpec;
+                                angular.forEach(specData, function (valueSpec) {
+                                    if(valueSpec.specializationId === value.specializationId) {
 
-                            var colorSpec;
-                            angular.forEach(specData, function (valueSpec) {
-                                if(valueSpec.specializationId === value.specializationId) {
-
-                                    colorSpec = valueSpec.color;
-                                }
+                                        colorSpec = valueSpec.color;
+                                    }
+                                });
+                                self.scope.eventsSchedule.push({
+                                    title:value.eventName,
+                                    id: value.eventId,
+                                    start: value.eventStartTime,
+                                    end:value.eventEndTime,
+                                    color: colorSpec,
+                                    stick: true,
+                                })
                             });
-                            self.scope.eventsSchedule.push({
-                                title:value.eventName,
-                                id: value.eventId,
-                                start: value.eventStartTime,
-                                end:value.eventEndTime,
-                                color: colorSpec,
-                                stick: true,
-                            })
+
                         });
 
-                    });
-
-                self.scope.uiConfig = {
-                    calendar: {
-                        minTime: "08:00:00",
-                        maxTime: "22:00:00",
-                        weekends: true,
-                        height: 650,
-                        firstDay: 1,
-                        locale: 'pl',
-                        lang: 'pl',
-                        timezone: 'local',
-                        allDaySlot: false,
-                        editable: true,
-                        header: {
-                            left: 'month,agendaWeek,agendaDay',
-                            center: 'title',
-                            right: 'prev,next today'
-                        },
-                        eventClick: self.scope.alertOnEventClick,
-                        eventDrop: self.scope.alertOnDrop,
-                        eventResize: self.scope.alertOnResize,
-                        // Select options
-                        selectable: true,
-                        selectHelper: true,
-                        unselectAuto: true,
-/*                        select: function (start, end, moment) {
-                            var title = prompt('Event Title:');
-                            var eventData;
-                            if (title) {
-                                eventData = {
-                                };
-                                self.scope.addEvent(eventData);
-                            }
-                        }*/
-                    }
-                };
+                    self.scope.uiConfig = {
+                        calendar: {
+                            minTime: "08:00:00",
+                            maxTime: "22:00:00",
+                            weekends: true,
+                            height: 650,
+                            firstDay: 1,
+                            locale: 'pl',
+                            lang: 'pl',
+                            timezone: 'local',
+                            allDaySlot: false,
+                            editable: true,
+                            header: {
+                                left: 'month,agendaWeek,agendaDay',
+                                center: 'title',
+                                right: 'prev,next today'
+                            },
+                            eventClick: self.scope.alertOnEventClick,
+                            eventDrop: self.scope.alertOnDrop,
+                            eventResize: self.scope.alertOnResize,
+                            // Select options
+                            selectable: true,
+                            selectHelper: true,
+                            unselectAuto: true,
+                            /*                        select: function (start, end, moment) {
+                                                        var title = prompt('Event Title:');
+                                                        var eventData;
+                                                        if (title) {
+                                                            eventData = {
+                                                            };
+                                                            self.scope.addEvent(eventData);
+                                                        }
+                                                    }*/
+                        }
+                    };
+                } else {
+                    self.scope.isSpecialization = false;
+                }
             });
 
         self.scope.addEvent = function (eventData) {
